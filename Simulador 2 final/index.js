ccce3ccd4a944d11e1547c4b62827786147c5789
5128joaocarlos-web <5128.joaocarlos@gmail.com>
2025-12-23 14:32:40 -0300

    Add files via upload

---
+++ Simulador 2 final/index.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const botao = document.getElementById("botao");
+
+    botao.addEventListener("click", function () {
+        alert("Seja bem-vindo(a) ao nosso mundo digital!!");
+    });
+
+});
